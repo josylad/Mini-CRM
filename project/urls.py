a53825edd4323dbff9be4cd234c75556a323bf39
Joseph Adediji <josylado@gmail.com>
2020-01-30 00:32:25 +0100

    Fix URL bug

--- project/urls.py
+++ project/urls.py
@@ -30,6 +30,6 @@ urlpatterns = [
         name='registration_register',
     ),
     path('accounts/', include('django_registration.backends.one_step.urls')),
-    path('login/', views.LoginView.as_view(template_name="registration/login.html"), name='login'), 
-    path('logout/', views.LogoutView.as_view(template_name="registration/logout.html"), name='logout'), 
+    path('login/', views.LoginView.as_view(template_name="django_registration/login.html"), name='login'), 
+    path('logout/', views.LogoutView.as_view(template_name="django_registration/logout.html"), name='logout'), 
 ]
