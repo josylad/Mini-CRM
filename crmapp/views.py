03f97930d09d3b7ca4fc4d7acd1e5127021c1871
Joseph Adediji <josylado@gmail.com>
2020-01-29 23:29:15 +0100

    Update index views

--- crmapp/views.py
+++ crmapp/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.templatetags.static import static
+from django.shortcuts import render, redirect, HttpResponseRedirect
+from django.http import HttpResponse, Http404
+import datetime as dt
+from .models import *
+
+
 
 # Create your views here.
+def index(request):
+    date = dt.date.today()
+    companies = Companies.get_allcompany()
+    
+    return render(request, 'index.html', {"date": date, "companies":companies})
+
